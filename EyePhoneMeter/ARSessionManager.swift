17f6e0a7b88ec1d3bbd0f179e5301429c6568a2c
Toshiki Takahashi <t.toshiki.app@gmail.com>
2026-02-26 13:35:38 +0900

    refactor: ARSessionManagerを微修正

--- EyePhoneMeter/ARSessionManager.swift
+++ EyePhoneMeter/ARSessionManager.swift
@@ -2,8 +2,8 @@ import ARKit
 import Observation
 
 @Observable
-class ARSessionManager: NSObject, ARSessionDelegate {
-    var distance: Float = 0
+final class ARSessionManager: NSObject, ARSessionDelegate {
+    var distance = 0
     
     func session(_ session: ARSession, didUpdate anchors: [ARAnchor]) {
         guard let faceAnchor = anchors.first(where: { $0 is ARFaceAnchor }) as? ARFaceAnchor else { return }
@@ -11,6 +11,6 @@ class ARSessionManager: NSObject, ARSessionDelegate {
         let position = faceAnchor.transform.columns.3
         let currentDistance = sqrt(pow(position.x, 2) + pow(position.y, 2) + pow(position.z, 2))
         
-        self.distance = currentDistance * 100
+        self.distance = Int(currentDistance * 100)
     }
 }
