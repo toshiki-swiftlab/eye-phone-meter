4c5d6cd85d60a27a80b035129cb89c21601cf6bc
Toshiki Takahashi <t.toshiki.app@gmail.com>
2026-02-26 23:32:56 +0900

    feat: TrueDepthが搭載されていない端末の対応

--- EyePhoneMeter/MeasurementView.swift
+++ EyePhoneMeter/MeasurementView.swift
@@ -1,19 +1,25 @@
 import SwiftUI
+import ARKit
 import RealityKit
 
 struct MeasurementView: View {
     @Environment(ARSessionManager.self) private var sessionManager
     
     var body: some View {
-        VStack {
-            if sessionManager.distance <= 0 {
-                Text("測定不能")
-            } else {
-                Text(String(sessionManager.distance))
+        // サポートしてないなら、終了
+        if ARFaceTrackingConfiguration.isSupported {
+            VStack {
+                if sessionManager.distance <= 0 {
+                    Text("測定不能")
+                } else {
+                    Text(String(sessionManager.distance))
+                }
+                ARViewContainer(sessionManager: sessionManager)
+                    .aspectRatio(3 / 4, contentMode: .fit)
+                    .frame(maxWidth: .infinity)
             }
-            ARViewContainer(sessionManager: sessionManager)
-                .aspectRatio(3 / 4, contentMode: .fit)
-                .frame(maxWidth: .infinity)
+        } else {
+            Text("ご利用の端末ではTrueDepthカメラが搭載されていません。")
         }
     }
 }
