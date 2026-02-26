c1ff462571e6606568fc33a9cb3bd4bf3be50293
Toshiki Takahashi <t.toshiki.app@gmail.com>
2026-02-26 23:23:07 +0900

    feat: 距離ラベルを追加

--- EyePhoneMeter/MeasurementView.swift
+++ EyePhoneMeter/MeasurementView.swift
@@ -5,9 +5,16 @@ struct MeasurementView: View {
     @Environment(ARSessionManager.self) private var sessionManager
     
     var body: some View {
-        ARViewContainer(sessionManager: sessionManager)
-            .aspectRatio(3 / 4, contentMode: .fit)
-            .frame(maxWidth: .infinity)
+        VStack {
+            if sessionManager.distance <= 0 {
+                Text("測定不能")
+            } else {
+                Text(String(sessionManager.distance))
+            }
+            ARViewContainer(sessionManager: sessionManager)
+                .aspectRatio(3 / 4, contentMode: .fit)
+                .frame(maxWidth: .infinity)
+        }
     }
 }
 
