b055ea204302a7b5228287a6cefcd1a413863d7f
Toshiki Takahashi <t.toshiki.app@gmail.com>
2026-03-01 22:13:47 +0900

    例外対応とUIの細分化

--- EyePhoneMeter/View/MeasurementView.swift
+++ EyePhoneMeter/View/MeasurementView.swift
@@ -3,60 +3,101 @@ import ARKit
 import RealityKit
 
 struct MeasurementView: View {
+    var body: some View {
+        if ARFaceTrackingConfiguration.isSupported {
+            switch AVCaptureDevice.authorizationStatus(for: .video) {
+            case .authorized:
+                ContentView()
+            case .denied:
+                UnavailableView(
+                    systemIconName: "camera",
+                    message: "カメラのアクセスを許可して下さい。"
+                )
+            case .restricted:
+                UnavailableView(
+                    systemIconName: "camera",
+                    message: "端末の設定により、カメラを使用できませんでした。"
+                )
+            case .notDetermined:
+                ProgressView()
+            default:
+                ContentView()
+            }
+        } else {
+            UnavailableView(
+                systemIconName: "person.and.background.dotted",
+                message: "ご利用のiPhoneにはTrueDepthカメラが搭載されていません。"
+            )
+        }
+    }
+}
+
+struct ContentView: View {
     
     @State private var eyePhoneMeter = EyePhoneMeter()
     
     @State private var settingsView = false
     
     var body: some View {
-        if ARFaceTrackingConfiguration.isSupported {
-            VStack {
-                Spacer()
-                switch eyePhoneMeter.status {
-                case .good, .tooClose:
-                    Text(eyePhoneMeter.status.description)
-                        .padding()
-                        .background(eyePhoneMeter.status.color.opacity(0.3))
-                        .clipShape(.capsule)
-                        .foregroundStyle(eyePhoneMeter.status.color)
-                        .bold()
-                    Text("判定：およそ\(eyePhoneMeter.distance)cm")
-                        .foregroundStyle(.secondary)
-                case .multiplePeople, .notTracking:
-                    Text(eyePhoneMeter.status.description)
-                        .font(.headline)
-                }
-                Spacer()
-                ARViewContainer(eyePhoneMeter: eyePhoneMeter)
-                    .aspectRatio(3 / 4, contentMode: .fit)
-                    .frame(maxWidth: .infinity)
-                HStack {
-                    Spacer()
-                    Button(
-                        action: onGearButton,
-                        label: {
-                            Image(systemName: "gearshape.fill")
-                                .font(.system(size: 24))
-                                .padding(8)
-                        }
-                    )
-                    .buttonStyle(.glass)
-                    .buttonBorderShape(.circle)
-                    Spacer()
-                }
-                .padding(.vertical)
+        VStack {
+            Spacer()
+            switch eyePhoneMeter.status {
+            case .good, .tooClose:
+                Text(eyePhoneMeter.status.description)
+                    .padding()
+                    .background(eyePhoneMeter.status.color.opacity(0.3))
+                    .clipShape(.capsule)
+                    .foregroundStyle(eyePhoneMeter.status.color)
+                    .bold()
+                Text("判定：およそ\(eyePhoneMeter.distance)cm")
+                    .foregroundStyle(.secondary)
+            case .multiplePeople, .notTracking:
+                Text(eyePhoneMeter.status.description)
+                    .font(.headline)
             }
-            .sheet(isPresented: $settingsView) {
-                SettingsView()
-                    .presentationDetents([.medium, .large])
+            Spacer()
+            ARViewContainer(eyePhoneMeter: eyePhoneMeter)
+                .aspectRatio(3 / 4, contentMode: .fit)
+                .frame(maxWidth: .infinity)
+            HStack {
+                Spacer()
+                Button(
+                    action: onGearButton,
+                    label: {
+                        Image(systemName: "gearshape.fill")
+                            .font(.system(size: 24))
+                            .padding(8)
+                    }
+                )
+                .buttonStyle(.glass)
+                .buttonBorderShape(.circle)
+                Spacer()
             }
-        } else {
-            Text("ご利用の端末ではTrueDepthカメラが搭載されていません。")
+            .padding(.vertical)
+        }
+        .sheet(isPresented: $settingsView) {
+            SettingsView()
+                .presentationDetents([.medium, .large])
         }
     }
     
     private func onGearButton() {
         settingsView = true
+    }
+}
+
+struct UnavailableView: View {
+    let systemIconName: String
+    let message: String
+    
+    var body: some View {
+        VStack {
+            Image(systemName: systemIconName)
+                .font(.title)
+            Text(message)
+                .bold()
+        }
+        .foregroundStyle(.secondary)
     }
 }
 
