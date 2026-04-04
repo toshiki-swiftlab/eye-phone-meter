6ee2548892e3620608d19be23b8f52b3fc8a4ae1
Toshiki Takahashi <t.toshiki.app@gmail.com>
2026-04-04 22:01:49 +0900

    実行順を変更

--- EyePhoneMeter/Manager/UserNotificationManager.swift
+++ EyePhoneMeter/Manager/UserNotificationManager.swift
@@ -11,18 +11,20 @@ final class UserNotificationManager: NSObject, UNUserNotificationCenterDelegate 
     }
     
     func add20mRepeatNotification() async throws {
-        let content = UNMutableNotificationContent()
-        content.title = "20分経過"
-        content.body = "目を休める時間です！🌱"
-        content.categoryIdentifier = UserNotificationCategoryID.timer
-        let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 60 * 20, repeats: true)
-        let request = UNNotificationRequest(identifier: UserNotificationID.repeat20m, content: content, trigger: trigger)
+        // category
         let category = UNNotificationCategory(
             identifier: UserNotificationCategoryID.timer,
             actions: [UNNotificationAction(identifier: UserNotificationActionID.stopTimer, title: "タイマー停止", icon: UNNotificationActionIcon(systemImageName: "stop.circle"))],
             intentIdentifiers: []
         )
         UNUserNotificationCenter.current().setNotificationCategories([category])
+        // request
+        let content = UNMutableNotificationContent()
+        content.title = "20分経過"
+        content.body = "目を休める時間です！🌱"
+        content.categoryIdentifier = UserNotificationCategoryID.timer
+        let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 60 * 20, repeats: true)
+        let request = UNNotificationRequest(identifier: UserNotificationID.repeat20m, content: content, trigger: trigger)
         try await UNUserNotificationCenter.current().add(request)
     }
     
